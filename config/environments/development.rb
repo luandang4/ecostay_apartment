039f28e5b248748f91c696ab132c7856be36a647
luandang4 <luandang4@gmail.com>
2022-11-17 10:44:00 +0700

    config ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.hosts << /[a-z0-9-.]+\.ngrok\.io/
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
