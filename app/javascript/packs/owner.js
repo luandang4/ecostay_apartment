8365bfefba7e004ea061c32e1397eff5e2f6125f
luandang4 <luandang4@gmail.com>
2022-12-11 11:01:43 +0700

    ECO_STAY-13

--- app/javascript/packs/owner.js
+++ app/javascript/packs/owner.js
@@ -6,6 +6,21 @@ $(document).ready(function(){
     $(".js-delete-service").submit();
   });
 
+  $(document).on('change', '.switch-language', function(){
+    debugger
+    if($(this).prop('checked')) {
+      path = window.location.pathname.slice(3)
+      path = '/en' + path
+      window.location.pathname = path
+      $(this).checked = false
+    } else {
+      path = window.location.pathname.slice(3)
+      path = '/vi' + path
+      window.location.pathname = path
+      $(this).checked = true
+    }
+  });
+
   setTimeout(function() {
     $('.alert-dismissible').hide()
   }, 3500);
