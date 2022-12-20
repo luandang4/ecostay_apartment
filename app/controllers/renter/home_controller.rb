ce93de8f81a115cdc459d2a66a85e4d3e6196bbb
luandang4 <luandang4@gmail.com>
2022-12-20 23:47:52 +0700

    ECO_STAY-19

--- app/controllers/renter/home_controller.rb
+++ app/controllers/renter/home_controller.rb
@@ -2,5 +2,7 @@ class Renter::HomeController < Renter::BaseController
   skip_before_action :verify_authenticity_token, only: :index
 
   def index
+    operation = Renter::Home::IndexOperation.new(params, current_user, session: session)
+    operation.call
   end
 end
