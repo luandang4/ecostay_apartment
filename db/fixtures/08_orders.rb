0a091dff4eb7eb94d232df5e4a57294219700704
luandang4 <luandang4@gmail.com>
2022-12-28 18:04:55 +0700

    Schedule

--- db/fixtures/08_orders.rb
+++ db/fixtures/08_orders.rb
@@ -2,4 +2,20 @@ Order.delete_all
 
 rooms = Room.where(status: 1)
 rooms.each do |room|
+  service_room = room.service_rooms
+  if service_room.present?
+
+    amout_service = 0
+  if service_room.present?
+    service_room.each do |service|
+      id = service.service_id
+      amout_service += Service.find(id).price
+    end
+  end
+
+    room.orders.create(
+      group_user_id:   room.group_user.id,
+      sum:             amout_service
+    )
+  end
 end
