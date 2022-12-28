e0346b405605003bf58a325a867ca64da4fc658c
luandang <luandang4@gmail.com>
2022-12-28 09:15:35 +0700

    aa

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,12 @@
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: "redis://127.0.0.1:6379/12"
+    url: ENV['REDIS_URL'] || 'redis://127.0.0.1:6379', namespace: 'sidekiq'
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: "redis://127.0.0.1:6379/12"
+    url: ENV['REDIS_URL'] || 'redis://127.0.0.1:6379', namespace: 'sidekiq'
   }
 end
 
